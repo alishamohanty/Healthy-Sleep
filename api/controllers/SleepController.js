dcf8d3a01ab83c2d2bb3999a160ee02d984a2cb3
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-06-26 19:00:02 +0530

    Find method created in SleepController.js

--- api/controllers/SleepController.js
+++ api/controllers/SleepController.js
@@ -25,6 +25,69 @@ module.exports = {
           
       });
     },
+    find: function (req,res,next)
+    {
+      console.log("We entered inside find method");
+      var id=req.param('id');
+      
+      if(id)
+      {
+          Sleep.findOne(id,function (err,sleep)
+          {
+            if(sleep === undefined)
+            {
+                res.notFound();
+            }   
+            if(err)
+            {
+                next(err);
+            }
+            res.json(sleep);
+            console.log('Inside findone method',sleep);
+          });      
+      }
+       else
+       {
+           var where=req.param('where');
+
+           if(_.isString(where))
+           {
+               where=JSON.parse(where);
+           }
+           var options=
+           {
+              limit: req.param('limit') || undefined ,
+              skip: req.param('skip') || undefined,
+              sort: req.param('sort') || undefined,
+              where: where || undefined
+           };
+           console.log('This is the options ', options);
+           Sleep.find(options, function(err, sleep)
+           {
+             if(sleep === undefined)
+             {
+                 res.notFound();
+                 console.log('We are in find methods error function');
+                 
+             }  
+             if(err)
+             {
+                 return next(err);
+             }
+             res.json(sleep);
+
+           });
+        }
+             function isShortcut(id)
+             {
+               if(id === 'find' || id === 'update' || id === 'create' || id === 'destroy') 
+               {
+                   return true;
+               }  
+             }
+    }
+
+  
     
 
 };
